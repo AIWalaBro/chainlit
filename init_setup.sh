365ff16654ff59b7c69ac76ebc4c11c2a53689d6
AIWalaBro <aiwalabro@gmail.com>
2024-05-04 11:32:56 +0530

    updated

--- init_setup.sh
+++ init_setup.sh
@@ -1,9 +1,9 @@
 echo [$(date)]: "START"
-echo [$(date)]: "creating a conda env with python 3.9"
-conda create --prefixt ./env python=3.9 -y
-echo [$(date)]: "created conda env"
+echo [$(date)]: "creating a conda env with python 3.10"
+conda create -p ./env python=3.10 -y
+echo [$(date)]: "created conda venv_chainlit"
 source activate ./env
-echo [$(date)]: "activated conda env"
+echo [$(date)]: "activated conda venv_chainlit"
 echo [$(date)]: "installing requirements"
 pip install -r requirements.txt
 echo [$(date)]: "installed all the requirement"
